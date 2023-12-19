b5b9dfd05b276725bca86c64c8ec285f8da4ac3f
honnuanand <anand@honnu.com>
2023-12-19 13:54:22 -0800

    small comment and test

--- src/main/java/io/clue2solve/spring/cloud/starter/huggingface/inference/service/HFModels.java
+++ src/main/java/io/clue2solve/spring/cloud/starter/huggingface/inference/service/HFModels.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+/**
+ * Hugging Face Models
+ */
 @Service
 public class HFModels {
 
@@ -14,6 +17,13 @@ public class HFModels {
 		this.restTemplate = restTemplate;
 	}
 
+	/**
+	 * Get all models
+	 * @return String
+	 * params:
+	 * pipeline_tag: str (the type of model : e.g., text-generation)
+	 * search: str (e.g., gpt2)
+	 */
 	public String getModelsByFilter(String modelType, String modelId) {
 		try {
 			UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://huggingface.co/api/models")
---
+++ src/test/java/io/clue2solve/spring/cloud/starter/huggingface/inference/starter/HFModelsTest.java
@@ -0,0 +1,48 @@
+package io.clue2solve.spring.cloud.starter.huggingface.inference.starter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.clue2solve.spring.cloud.starter.huggingface.inference.autoconfigure.RestTemplateAutoConfiguration;
+import io.clue2solve.spring.cloud.starter.huggingface.inference.service.HFModels;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = { TestInit.class, HFModels.class, RestTemplateAutoConfiguration.class })
+public class HFModelsTest {
+
+	Logger logger = LoggerFactory.getLogger(HFModelsTest.class);
+
+	@Autowired
+	private HFModels hfModels;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Test
+	public void testGetModelsByFilter() throws Exception {
+		String response = hfModels.getModelsByFilter("text-classification", "GPT3");
+
+		// Assert that the response is not null
+		assertNotNull(response);
+
+		// Parse the response into a JsonNode
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(response);
+
+		// Pretty print the JsonNode
+		String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
+		logger.info("Response: {}", prettyJson);
+
+		// Assert that the JsonNode is an array
+		assertTrue(node.isArray());
+	}
+
+}
